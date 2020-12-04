439209ca445cf31d844278f8cd6d36f4508a0208
kdnakt <a.kid.1985@gmail.com>
2020-12-04 09:32:48 +0900

    6.3

---
+++ code6.3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Game start!" << endl;
+
+    int left = 20, right = 36;
+    while (right - left > 1) {
+        int mid = left + (right - left) / 2;
+
+        cout << "Is the age less than " << mid << " ? (yes / no)" << endl;
+        string ans;
+        cin >> ans;
+        if (ans == "yes") right = mid;
+        else left = mid;
+    }
+
+    cout << "The age is " << left << "!" << endl;
+}
